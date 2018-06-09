b086f2f14047bac34dac6d35dbaea54b4cf6ccef
Raman Shalupau <raman.shalupau@gmail.com>
2018-06-09 15:50:53 +0800

    adding header

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -17,17 +17,21 @@ const genders = [{
 class Home extends React.Component {
   render() {
     return (
-      <Container>
-        <Header as='h1' content='ID PASS' />
+      <Container {...{style: {marginTop: '5em'}}}>
+        <Header as='h1' content='🔑 ID PASS' />
         <Form>
           <Form.Group>
-            <Form.Input label='Name' />
+            <Form.Input label='Full Legal Name' />
+            <Form.Input label='Short Name' />
           </Form.Group>
           <Form.Group>
-            <Form.Dropdown label='Pick your gender' selection options={genders} />
+            <Form.Dropdown label='Gender' selection options={genders} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input label='Date of Birth' />
           </Form.Group>
         </Form>
-        <Button color='green' content='Save' />
+        <Button color='green' content='Print' />
       </Container>
     )
   }
