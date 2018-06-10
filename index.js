f9011c5084182ca18f79ebb57a1e903b3678a378
Raman Shalupau <raman.shalupau@gmail.com>
2018-06-10 08:03:57 +0800

    signing endpoint

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
 const express = require('express')
 const app = express()
+const _ = require('lodash')
 
 const jsonld = require('jsonld')
 const jsig = require('jsonld-signatures')
 jsig.use('jsonld', jsonld)
 
+const privateKeyWif = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+const publicKey = 'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
+
+let baseInput = {
+  '@context': [
+    'https://w3id.org/identity/v1',
+    'https://w3id.org/credentials/v1',
+    'http://schema.org/'
+  ],
+  'id': 'ASD',
+  'type': ['Person'],
+  'name': 'name',
+  'birthDate': 'NAME'
+}
+
+app.get('/sign', (req, res) => {
+  console.log(req.query)
+  let input = _.defaults(req.query, baseInput)
+  jsig.sign(input, {
+    algorithm: 'EcdsaKoblitzSignature2016',
+    privateKeyWif: privateKeyWif,
+    creator: 'https://example.com/i/alice/keys/1'
+  }, (err, signedDocument) => {
+    if (err) {
+      console.log('Signing error:', err);
+      return res.send(500);
+    }
+    console.log('Signed document:', signedDocument)
+    return res.send(signedDocument);
+  });
+})
+
+app.get('/verify', (req, res) => {
+  console.log(req.query)
+  jsig.verify(signedDocument, {
+    algorithm: 'EcdsaKoblitzSignature2016',
+    publicKeyWif: publicKey,
+    testPublicKeyOwner: {
+      "@context": jsig.SECURITY_CONTEXT_URL,
+      '@id': 'https://example.com/i/alice',
+      publicKey: [publicKey]
+    }
+  }, (err, verified) => {
+    if (err) {
+      return res.send('Signature verification error:' + err);
+    }
+    return res.send('Signature is valid:', verified, verified.keyResults[0]);
+  });
+})
 
 app.get('/', (req, res) => {
   console.log(req.query)
   res.send('hello world')
 })
+
 
 const port = 3000;
 app.listen(port, () => {
